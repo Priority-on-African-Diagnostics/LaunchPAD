91e8d7a4e2d82aed8c525b76ece4420915744249
Thomas Crocker <thomas.crocker@metoffice.gov.uk>
2021-07-26 14:52:15 +0100

    both plots types now working

--- ESMValTool/diag_scripts/AEJ.py
+++ ESMValTool/diag_scripts/AEJ.py
@@ -4,8 +4,10 @@ Diagnostic script for LaunchPAD AEJ.
 
 # to manipulate iris cubes
 import iris
+import iris.quickplot as qplt
 import xarray as xr
 
+import matplotlib
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
@@ -14,6 +16,22 @@ import numpy as np
 # import internal esmvaltool modules here
 from esmvaltool.diag_scripts.shared import group_metadata, run_diagnostic
 from esmvalcore.preprocessor import climate_statistics, zonal_statistics
+
+obs_list = ["ERA5", "MERRA2"]
+mon_names = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+]
 
 
 def prep_data(cfg):
@@ -119,20 +137,6 @@ def plot_TG(ds_list):
 
     cmap = cm.get_cmap("coolwarm")
     clevs = np.arange(-5, 6, 1)
-    mon_names = [
-        "Jan",
-        "Feb",
-        "Mar",
-        "Apr",
-        "May",
-        "Jun",
-        "Jul",
-        "Aug",
-        "Sep",
-        "Oct",
-        "Nov",
-        "Dec",
-    ]
 
     for ds in ds_list:
         LN = ds_list[ds]["N_lat"]
@@ -169,10 +173,174 @@ def plot_TG(ds_list):
         plt.colorbar(cf, colorbar_axes, orientation="vertical")
 
         plt.savefig(
-            config["plot_dir"] + "TG_plot_" + ds + "_AEJ_plot.png",
+            config["plot_dir"] + "/TG_plot_" + ds + "_AEJ_plot.png",
             bbox_inches="tight",
             dpi=100,
         )
+
+
+def plot_AEJ(ds_list):
+    """Create AEJ plots
+
+    Args:
+        ds_list (dict): Prepped data
+    """
+
+    len_clist = int(np.ceil(len(ds_list.keys()) / 3.0))
+    cm = plt.get_cmap("gist_rainbow", len_clist)
+
+    colourWheel = []
+    for i in range(len_clist):
+        rgb = cm(i)[:3]  # will return rgba, we take only first 3 so we get rgb
+        colourWheel.append(str(matplotlib.colors.rgb2hex(rgb)))
+
+    fig, ax = plt.subplots()
+    nameOfPlot = "African Easterly Jet (Giresse Turin)"
+    fig.suptitle(nameOfPlot, fontsize=14)
+    plt.title(nameOfPlot)
+
+    clist = len(ds_list.keys())
+
+    dashesStyles = [[3, 1], [1000, 1], [2, 1, 10, 1], [4, 1, 1, 1, 1, 1]]
+
+    plt.subplot(2, 2, 1)
+    plt.title("North")
+    for j, expt in enumerate(ds_list.keys()):
+        LN = ds_list[expt]["N_lat"]
+        if expt in obs_list:
+            c_o = "#000000"
+            linethick = 2.5
+            c_zorder = clist + 1
+            dashesSty = dashesStyles[obs_list.index(expt)]
+        else:
+            c_o = colourWheel[j % len(colourWheel)]
+            linethick = 1.5
+            c_zorder = j
+            if j >= len_clist and j < 2 * len_clist:
+                dashesSty = dashesStyles[0]
+            elif j >= 2 * len_clist:
+                dashesSty = dashesStyles[2]
+            else:
+                dashesSty = dashesStyles[1]
+        plt.plot(
+            mon_names,
+            LN,
+            linestyle="-",
+            color=c_o,
+            dashes=dashesSty,
+            lw=linethick,
+            label=expt,
+            zorder=c_zorder,
+        )
+    plt.ylim(0, 14)
+    plt.xticks(np.arange(12), mon_names, rotation=45)
+    plt.ylabel("Latitude", fontsize=12)
+
+    plt.subplot(2, 2, 2)
+    plt.title("South")
+    for j, expt in enumerate(ds_list.keys()):
+        LS = ds_list[expt]["S_lat"]
+        if expt in obs_list:
+            dashesSty = dashesStyles[obs_list.index(expt)]
+            c_o = "#000000"
+            linethick = 2.5
+            c_zorder = clist + 1
+        else:
+            c_o = colourWheel[j % len(colourWheel)]
+            linethick = 1.5
+            c_zorder = j
+            if j >= len_clist and j < 2 * len_clist:
+                dashesSty = dashesStyles[0]
+            elif j >= 2 * len_clist:
+                dashesSty = dashesStyles[2]
+            else:
+                dashesSty = dashesStyles[1]
+        plt.plot(
+            mon_names,
+            LS,
+            linestyle="-",
+            color=c_o,
+            dashes=dashesSty,
+            lw=linethick,
+            label=expt,
+            zorder=c_zorder,
+        )
+    plt.ylim(-12, 0)
+    plt.xticks(np.arange(12), mon_names, rotation=45)
+
+    plt.subplot(2, 2, 3)
+    for j, expt in enumerate(ds_list.keys()):
+        CU_N = ds_list[expt]["N_intensity"]
+        if expt in obs_list:
+            c_o = "#000000"
+            linethick = 2.5
+            c_zorder = clist + 1
+            dashesSty = dashesStyles[obs_list.index(expt)]
+        else:
+            c_o = colourWheel[j % len(colourWheel)]
+            linethick = 1.5
+            c_zorder = j
+            if j >= len_clist and j < 2 * len_clist:
+                dashesSty = dashesStyles[0]
+            elif j >= 2 * len_clist:
+                dashesSty = dashesStyles[2]
+            else:
+                dashesSty = dashesStyles[1]
+        qplt.plot(
+            CU_N.coord("month_number"),
+            CU_N,
+            linestyle="-",
+            color=c_o,
+            dashes=dashesSty,
+            lw=linethick,
+            label=expt,
+            zorder=c_zorder,
+        )
+    plt.ylim(-6, -16)
+    plt.title(" ")
+    plt.xticks(np.arange(12), mon_names, rotation=45)
+    plt.ylabel("Intensity (m/s)", fontsize=12)
+
+    plt.subplot(2, 2, 4)
+    for j, expt in enumerate(ds_list.keys()):
+        CU_S = ds_list[expt]["S_intensity"]
+        if expt in obs_list:
+            c_o = "#000000"
+            linethick = 2.5
+            c_zorder = clist + 1
+            dashesSty = dashesStyles[obs_list.index(expt)]
+        else:
+            c_o = colourWheel[j % len(colourWheel)]
+            linethick = 1.5
+            c_zorder = j
+            if j >= len_clist and j < 2 * len_clist:
+                dashesSty = dashesStyles[0]
+            elif j >= 2 * len_clist:
+                dashesSty = dashesStyles[2]
+            else:
+                dashesSty = dashesStyles[1]
+        qplt.plot(
+            CU_S.coord("month_number"),
+            CU_S,
+            linestyle="-",
+            color=c_o,
+            dashes=dashesSty,
+            lw=linethick,
+            label=expt,
+            zorder=c_zorder,
+        )
+
+    plt.ylim(-6, -16)
+    plt.title(" ")
+    plt.xticks(np.arange(12), mon_names, rotation=45)
+
+    handles, labels = ax.get_legend_handles_labels()
+    plt.subplots_adjust(bottom=0.1, right=0.8, top=0.9)
+    plt.legend(title="Dataset", bbox_to_anchor=(1.05, 2.3), loc="upper left")
+
+    plt.savefig(
+        config["plot_dir"] + "/AEJ_plot_all_AEJ_plot.png", bbox_inches="tight", dpi=100
+    )
 
 
 if __name__ == "__main__":
@@ -183,3 +351,4 @@ if __name__ == "__main__":
         data_for_plotting = prep_data(config)
         data_for_plotting = calc_for_plotting(data_for_plotting)
         plot_TG(data_for_plotting)
+        plot_AEJ(data_for_plotting)
